e6bad176bf14bc58aa057ce24068affc1d52f7cb
Diego Soto <113551815+dsoto39@users.noreply.github.com>
2022-10-05 19:18:52 +0000

    Loops

--- loops.py
+++ loops.py
@@ -137,8 +137,18 @@ def loop():
   
   # num % 2 == 1 (odd values)
   
-  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  #list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  # sum_even = 0
   
+  # sum_odd = 0
+
+  # for num in list_numbers:
+  #   if num % 2 == 0:
+  #     sum_even + sum_even + num
+  #   elif num % 2 == 1:
+  #     sum_odd = sum_odd + num
+  # print(sum_even)
+  # print(sum_odd)
   # sum_even = 
   
   # sum_odd = 
