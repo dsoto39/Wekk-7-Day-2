ed359d70f779ee4d810a8db14bc466fceddc6256
Diego Soto <113551815+dsoto39@users.noreply.github.com>
2022-10-05 19:05:50 +0000

    Loops

--- loops.py
+++ loops.py
@@ -107,7 +107,8 @@ def loop():
   # For example: "Hello Norville"
   
   students = ["Norville", "Fred", "Velma", "Daphne"]
-  
+  for name in students:
+    print(f"hello{name}")
   
   
   
@@ -125,6 +126,10 @@ def loop():
   # Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
   
   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  sum_numbers = 0
+  for number in list_numbers:
+    sum_numbers = sum_numbers + number
+    print(f"sum of list_numbers is {sum_numbers}")
   
   # *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
   
